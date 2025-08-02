6b76624516580f6f5015b434d56616b7084deb4a
surajkhatri333 <surajkhatr1234567@gmail.com>
2025-08-02 22:50:41 +0530

    initail commit

---
+++ automobiles/brand.js
@@ -0,0 +1,8 @@
+const brands = [
+    "https://upload.wikimedia.org/wikipedia/commons/thumb/3/3e/Ford_logo_flat.svg/1200px-Ford_logo_flat.svg.png",
+    "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTpfbnsY8Rzb61ychauiWlcduDxcNtM8i6hHg&s",
+    "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSbtzShoA3u0KU4W7IqL7JJnMJi8rk7MJrJKA&s",
+    "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR-DU03VqJBKs-eWcIBLTfqjIWvLHD6Iw1fjw&s",
+    "https://gemo.de/wp-content/uploads/2022/04/gemo-krefeld-logo.png"
+];
+export default brands;
---
+++ automobiles/carParts.js
@@ -0,0 +1,24 @@
+const carParts = [
+    { name: "Alloy Wheel", price: "$250", image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTgBfxXSEPknR5u3RtV1jXAi5-uLSfN2jDcfw&s" },
+    { name: "Brake Disc", price: "$120", image: "https://image.made-in-china.com/202f0j00yFCtRkoKMVqS/High-Performance-Brake-Discs-for-Auto-Cars.jpg" },
+    { name: "Engine Cover", price: "$300", image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSvc3TF_u0TrUG0WeNhP8_I57v-Q9STYZJ0EQ&s" },
+    { name: "Exhaust Tip", price: "$90", image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSP2QR_nxugrtyeM9ACogz0rY7W5ZisrQzJWg&s" },
+    { name: "Turbo Charger", price: "$450", image: "https://turboturbos.com/cdn/shop/articles/4eWj21svKe9cIQGORToPXi4w0UeIxott1608233063_1200x1200.jpg?v=1610326491" },
+    { name: "LED Headlight", price: "$200", image: "https://5.imimg.com/data5/SELLER/Default/2022/6/JN/SH/LU/116222445/car-headlight-500x500-500x500.jpg" },
+    { name: "Steering Wheel", price: "$180", image: "https://www.shutterstock.com/image-photo/modern-car-interior-steering-wheel-600nw-2328150279.jpg" },
+    { name: "Gearbox", price: "$650", image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTNlxWLdgWn5DFq6YGcmTkxAifc2hkpVzWdeg&s" },
+
+
+
+
+
+    { name: "Alloy Wheel", price: "$250", image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTgBfxXSEPknR5u3RtV1jXAi5-uLSfN2jDcfw&s" },
+    { name: "Brake Disc", price: "$120", image: "https://image.made-in-china.com/202f0j00yFCtRkoKMVqS/High-Performance-Brake-Discs-for-Auto-Cars.jpg" },
+    { name: "Engine Cover", price: "$300", image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSvc3TF_u0TrUG0WeNhP8_I57v-Q9STYZJ0EQ&s" },
+    { name: "Exhaust Tip", price: "$90", image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSP2QR_nxugrtyeM9ACogz0rY7W5ZisrQzJWg&s" },
+    { name: "Turbo Charger", price: "$450", image: "https://turboturbos.com/cdn/shop/articles/4eWj21svKe9cIQGORToPXi4w0UeIxott1608233063_1200x1200.jpg?v=1610326491" },
+    { name: "LED Headlight", price: "$200", image: "https://5.imimg.com/data5/SELLER/Default/2022/6/JN/SH/LU/116222445/car-headlight-500x500-500x500.jpg" },
+    { name: "Steering Wheel", price: "$180", image: "https://www.shutterstock.com/image-photo/modern-car-interior-steering-wheel-600nw-2328150279.jpg" },
+    { name: "Gearbox", price: "$650", image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTNlxWLdgWn5DFq6YGcmTkxAifc2hkpVzWdeg&s" }
+];
+export default carParts;
